73935c485f5ec89247cc6497c0d0d677fdd96b92
Moritz Gellner <moritz.gellner@gmail.com>
2014-04-16 17:00:37 -0500

    Add command line parser and updated config files

---
+++ photo-wall/Options.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using CommandLine.Text;
+
+namespace photo_wall
+{
+    class Options
+    {
+        [Option('d', "debug", DefaultValue = false, HelpText = "Whether to print debug messages.")]
+        public Boolean Debug  { get; set; }
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage() {
+            return HelpText.AutoBuild(this,
+                (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
--- photo-wall/Program.cs
+++ photo-wall/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CommandLine;
 
 namespace photo_wall
 {
@@ -9,6 +10,11 @@ namespace photo_wall
     {
         static void Main(string[] args)
         {
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args,options)) {
+                Console.WriteLine("Debug is set to {0}!", options.Debug);
+            }
+            Console.ReadLine();
         }
     }
 }
